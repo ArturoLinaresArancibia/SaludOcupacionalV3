bfe97915d8d228a88890b4b027509fd57dc32fd8
ArturoLinaresArancibia <a.linares@outlook.com>
2025-10-16 15:06:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Rellena con tus credenciales de Supabase
-export const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-export const SUPABASE_ANON_KEY = "TU_ANON_KEY";
+export const SUPABASE_URL = "https://pcxjcfiawsetysrmgtao.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjeGpjZmlhd3NldHlzcm1ndGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDI0MjEsImV4cCI6MjA3NjA3ODQyMX0.5eDi8nyGNGifKbfsBjxiyCTwK8CitF2lQAmdOQs271M";
 // (opcional) portal externo público
-export const EXTERNAL_PORTAL_URL = "https://artlincl.github.io";
+export const EXTERNAL_PORTAL_URL = "https://arturolinaresarancibia.github.io/Mapa-de-riesgo-DRT";
